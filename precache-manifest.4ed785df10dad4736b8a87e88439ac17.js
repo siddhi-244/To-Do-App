2452a77d1d33074b7fec37c3f4ceebac66f93298
Siddhi-244 <siddhibhanushali1234@gmail.com>
2022-07-29 16:15:57 +0530

    Updates

---
+++ precache-manifest.4ed785df10dad4736b8a87e88439ac17.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d46c7c9da63a762750a4ae6005689072",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "a6f07b8597acb3dc1650",
+    "url": "/To-Do-App/static/js/2.a6f19205.chunk.js"
+  },
+  {
+    "revision": "e5b65ef61ab6001c6352",
+    "url": "/To-Do-App/static/js/main.98b98c9a.chunk.js"
+  },
+  {
+    "revision": "011c8ac8ec6a3a0211ee",
+    "url": "/To-Do-App/static/js/runtime-main.f1ab0e03.js"
+  }
+]);
